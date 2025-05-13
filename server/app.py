2cc97e8659c51c2ff1de46b61ea88104d074601c
Clayvan Omondi <claydrinch@gmail.com>
2025-05-13 07:46:24 -0700

    new

--- server/app.py
+++ server/app.py
@@ -1,6 +1,3 @@
-# server/app.py
-#!/usr/bin/env python3
-
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
@@ -9,17 +6,30 @@ from models import db, Pet
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
+    body = {'message': 'Welcome to the pet directory!'}
+    return make_response(body, 200)
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        body = {'id': pet.id,
+                'name': pet.name,
+                'species': pet.species}
+        status = 200
+    else:
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
 
 @app.route('/species/<string:species>')
 def pet_by_species(species):
